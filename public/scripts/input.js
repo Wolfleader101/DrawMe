d2af358fbe96bf0b0e594ee9f8ad9cf076bf83bb
Wolfleader101 <james.morgan@iinet.net.au>
2019-06-18 14:38:02 +0800

    Update input.js

--- public/scripts/input.js
+++ public/scripts/input.js
@@ -1,32 +1,33 @@
-
-
-
-
 function SubmitInput() {
   let Input = document.getElementById("DrawInput").value;
-  let acceptedWords = ['Draw', 'Create', 'Make',]
+  const acceptedWords = ['draw', 'create', 'make', 'square', 'destroy'];
+  const commands = ['draw', 'create', 'make', 'destroy'];
+  const arguments = ['square'];
 
-  Input = Input.split(' ').filter(word => !acceptedWords.includes(word))
+  // Filter out the useless words. FilteredWords is now the words we keep
+  FilteredWords = Input.split(' ').filter(word => acceptedWords.includes(word));
+  console.log(FilteredWords); // make sure its working
 
-  console.log(Input);
-  
-  if (Input.toLowerCase() === "draw" || Input.toLowerCase() === "create") {
-    if (Input.toLowerCase() === "square") {
+//convert filtered words to lowercase
+FilteredWords.map(v => v.toLowerCase());
 
-      square.DrawSquare();
+  // check what the command is (e.g draw) then the argument (e.g square)
+    var Usercmds = FilteredWords.filter(x => commands.includes(x));
+    console.log(Usercmds);
 
-    }  else {
+  //check for first command then set variable
+  //check for first argument set variable
+    //run the command (e.g Draw the square)
+  //Now delete those words from array using  .shift()
+  //loop until no words left.
+  //Each command needs an argument and aeach argument requires a command otherwise delete
 
-      alert("Incorrect Format")
-    }
-  } else {
-
-    alert("Follow the guide!")
-  }
 }
 
 
 
+
+// DO NOT USE
 /* 
 
 let command, argument;
