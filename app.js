ff6c8e05eaccf9366901657b7ec5bb339443b5ec
Wolfleader101 <james.morgan@iinet.net.au>
2019-10-26 15:40:44 +0800

    Added Github Pages support

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const path = require('path');
 const port = 3302;
 
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/public/index.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/docs/index.html')));
 app.use(express.static(path.join(__dirname, 'public')));
 app.listen(`${port}`);
 
