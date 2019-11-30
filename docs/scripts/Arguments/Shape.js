5523ff310123ec7428b63ba1f73eb411969129d1
Wolfleader101 <james.morgan@iinet.net.au>
2019-11-30 20:45:22 +0800

    ## [0.3.3] - 2019-11-30
    ## [0.3.3] - 2019-11-30
    ### Added
    - Refactored code
    - Made Shape Class

--- docs/scripts/Arguments/Shape.js
+++ docs/scripts/Arguments/Shape.js
@@ -1,18 +1,41 @@
-const shapes = ['square', 'circle', 'rectangle'];
+class Shape {
+  cmd;
+  args;
 
-function DetermineArgument() {
+  DetermineCommand() {
+    // Run loop until all commands done
+    for(this.cmd of Usercmds) {
 
-  // Run loop to check all arguments
-  for(args of UserArgument) {
+      // Check if User Commands are create
+      if(Create.includes(this.cmd)) {
+        // check the argument
+        this.DetermineArgument();
 
-     //check the users shape
-    if (shapes.includes(args)) {
-      ShapeObjects[args].Draw();
+        // if they aren't check if they are destroy
+      } else if(Destroy.includes(this.cmd)) {
+        //check the argument
+        this.DetermineArgument();
 
-    } else {
-
-      alert('ERROR USER COMMAND NOT RECOGNIZED');
-      console.log('ERROR USER COMMAND NOT RECOGNIZED');
+      } else {
+        alert(`Error: The User Command ${this.cmd} was not found`);
+        console.log("\n" + `%cError: The User Command ${this.cmd} was not found`, "color: red;");
+      }
     }
   }
+  DetermineArgument() {
+    // Run loop to check all arguments
+    for(this.args of UserArgument) {
+
+      //check the users shape
+      if (shapes.includes(this.args)) {
+        ShapeObjects[this.args].Draw();
+
+      } else {
+
+        alert(`Error: The User Argument ${this.args} was not found`);
+        console.log("\n" + `%cError: The User Argument ${this.args} was not found`, "color: red;");
+      }
+    }
+  }
+
 }
--- docs/scripts/input.js
+++ docs/scripts/input.js
@@ -1,49 +1,35 @@
 // Defined so other scripts can access these variables
 var UserArgument;
 var Usercmds;
-var arguments;
+var shapes;
 var commands;
 
 function SubmitInput() {
   let Input = document.getElementById("DrawInput").value;
   Input = Input.toLowerCase();
-  commands = ['draw', 'create', 'make', 'destroy', 'delete'];
-  arguments = ['square', 'circle', 'rectangle'];
-  const acceptedWords = commands.concat(arguments);
+  commands = Create.concat(Destroy);
+  shapes = ['square', 'circle', 'rectangle'];
+  const acceptedWords = commands.concat(shapes);
 
   // Filter out the useless words. FilteredWords is now the words we keep
-  FilteredWords = Input.split(' ').filter(word => acceptedWords.includes(word));
+  let FilteredWords = Input.split(' ').filter(word => acceptedWords.includes(word));
   console.log("%cThe Filtered words:", "color: aqua;");  
   console.log(FilteredWords); // make sure its working
 
   // check the users commands (e.g draw)
-  Usercmds = FilteredWords.filter(x => commands.includes(x));
+  Usercmds = FilteredWords.filter(cmd => commands.includes(cmd));
     console.log("\n" + "%cThe User's Commands", "color: aqua;");
     console.log(Usercmds); // make sure its working
 
   // check the users arguments (e.g square)
-  UserArgument = FilteredWords.filter(y => arguments.includes(y));
+  UserArgument = FilteredWords.filter(arg => shapes.includes(arg));
   console.log("\n" + "%cThe User's Arguments", "color: aqua;");
   console.log(UserArgument); // make sure its working
-    
-  // Run loop until all commands done
-    for(cmd of Usercmds) {
 
-      // Check if User Commands are create
-      if(Create.includes(cmd)) {
-        // check the argument
-        DetermineArgument();
-        
-      // if they aren't check if they are destroy
-      } else if(Destroy.includes(cmd)) {
-        //check the argument
-        DetermineArgument();
+  // Get shape class
+  let shape = new Shape();
+  shape.DetermineCommand();
 
-      } else {
-        alert('ERROR USER COMMAND NOT RECOGNIZED');
-        console.log('ERROR USER COMMAND NOT RECOGNIZED');
-      }
-    }
    //run the command (e.g Draw the square)
   //Now delete those words from array using  .shift()
   //Each command needs an argument and each argument requires a command otherwise delete
